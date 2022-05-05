a49fbfb9b3ea25f218724bac612c953041bb6c7d
Saheme <roj_t@mail.ru>
2022-05-05 22:00:16 +0300

    full working version

--- src/Lesson6/HomeWork6.java
+++ src/Lesson6/HomeWork6.java
@@ -5,16 +5,18 @@ public class HomeWork6 {
     public static void main(String[] args) {
         int rightAnswer = 0;
         int wrongAnswer = 0;
-        String[] question = {"Столица Канады", "С каким океаном не граничит Канада"};
+        String[] question = {"Столица Канады", "С каким океаном не граничит Канада","Самая длинная река Канады","Самая высокая гора в Канаде"};
         for (int i = 0; i < question.length; i++) {
             System.out.println(question[i]);
-            String[] variant = {"Ванкувер", "Торонто", "Оттава", "Монреаль", "Атлантический океан", "Тихий океан", "Южный океан", "Северный Ледовитый океан"};
+            String[] variant = {"Ванкувер", "Торонто", "Оттава", "Монреаль", "Атлантический океан", "Тихий океан", "Южный океан", "Северный Ледовитый океан","Юкон","Колумбия","Маккензи","Пис","Вуд","Кинг Пик","Ванкувер","Логан"};
             for (int j = i * 4; j < i * 4 + 4; j++) {
                     int q=j+1;
-                    if(j>=4){q=1+j-4;}
-                    System.out.println(q + " " + variant[j]);
+                    if (j>=4) {q=1+j-4;}
+                    if (j>=8){q=1+j-8;}
+                    if (j>=12){q=1+j-12;}
+                System.out.println(q + " " + variant[j]);
             }
-            int[] answer = {3, 3};
+            int[] answer = {3, 3, 1, 4};
 
                 Scanner scan = new Scanner(System.in);
                 int input = scan.nextInt();
