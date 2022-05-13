a5a9b2360eac5709e84cd01a461a3e389dca7456
Saheme <roj_t@mail.ru>
2022-05-14 01:53:16 +0300

    First commit with array object

---
+++ src/Lesson8/Data.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+public class Data {
+    String [] question;
+    String[][] answer;
+    int [] rightAnswer;
+}
---
+++ src/Lesson8/HomeWork8.java
@@ -0,0 +1,19 @@
+package Lesson8;
+
+public class HomeWork8 {
+    public static void main(String[] args) {
+        Data question = new Data();
+        Data answer = new Data();
+        Data rightAnswer = new Data();
+        question.question = new String[]{"Столица Канады", "С каким океаном не граничит Канада",
+                "Самая длинная река Канады", "Самая высокая гора в Канаде"};
+        answer.answer = new String[][] {{"Ванкувер", "Торонто", "Оттава", "Монреаль"},
+                {"Атлантический океан", "Тихий океан", "Южный океан", "Северный Ледовитый океан"},
+                {"Юкон", "Колумбия", "Маккензи", "Пис"},
+                {"Вуд", "Кинг Пик", "Ванкувер", "Логан"}};
+        }
+
+
+
+    }
+
