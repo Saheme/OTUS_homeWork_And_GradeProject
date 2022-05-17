b03fb273697e7e3df83660c04120299df8cfd0f6
Saheme <roj_t@mail.ru>
2022-05-17 19:46:09 +0300

    Коммит с одним объектом

--- src/Lesson8/HomeWork8.java
+++ src/Lesson8/HomeWork8.java
@@ -4,35 +4,35 @@ import java.util.Scanner;
 
 public class HomeWork8 {
     public static void main(String[] args) {
-        Data question = new Data();
+        Data test = new Data();
+       /* Data question = new Data();
         Data variant = new Data();
-        Data rightAnswer = new Data();
+        Data rightAnswer = new Data(); */
 
-        question.question = new String[]{"Столица Канады", "С каким океаном не граничит Канада",
+        test.question = new String[]{"Столица Канады", "С каким океаном не граничит Канада",
                 "Самая длинная река Канады", "Самая высокая гора в Канаде"};
 
-        variant.variant = new String[][]{{"Ванкувер", "Торонто", "Оттава", "Монреаль"},
+        test.variant = new String[][]{{"Ванкувер", "Торонто", "Оттава", "Монреаль"},
                 {"Атлантический океан", "Тихий океан", "Южный океан", "Северный Ледовитый океан"},
                 {"Юкон", "Колумбия", "Маккензи", "Пис"},
                 {"Вуд", "Кинг Пик", "Ванкувер", "Логан"}};
 
-        rightAnswer.rightAnswer = new int[]{3, 3, 1, 4};
+        test.rightAnswer = new int[]{3, 3, 1, 4};
 
         int countRightAnswer = 0;
         int countWrongAnswer = 0;
 
-        for (int i = 0; i < question.question.length; i++) {
-            System.out.println(question.question[i]);
+        for (int i = 0; i < test.question.length; i++) {
+            System.out.println(test.question[i]);
 
-            for (int j = 0; j < question.question.length; j++) {
+            for (int j = 0; j < test.question.length; j++) {
                 int q = j + 1;
-                System.out.println(q + " " + variant.variant[i][j]);
+                System.out.println(q + " " + test.variant[i][j]);
             }
-            //int[] answer = {3, 3, 1, 4};
-
+            // Сканер и счетчик
             Scanner scan = new Scanner(System.in);
             int input = scan.nextInt();
-            if (input == rightAnswer.rightAnswer[i]) {
+            if (input == test.rightAnswer[i]) {
                 System.out.println("Вы абсолютно правы! ");
                 countRightAnswer++;
             } else {
