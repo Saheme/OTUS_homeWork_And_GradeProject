37e5586d4fc4d63cc628dc164e87ce9b483f9337
Saheme <roj_t@mail.ru>
2022-05-25 14:50:15 +0300

    задаются 2 массива случайными числами

--- src/Lesson11/ArrayTest.java
+++ src/Lesson11/ArrayTest.java
@@ -2,7 +2,16 @@ package Lesson11;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        Numbers array1 = new Numbers(new int[10]);
+        Numbers array1 = new Numbers(new int[3]);
+        for (int i = 0; i <array1.array.length; i++){
+            array1.array[i] =(int)(Math.random() * 10);
+            System.out.print(array1.array[i]);
+        }
+        Numbers array2 = new Numbers(new int[5]);
+        for (int j = 0; j <array2.array.length; j++){
+            array2.array[j] =(int)(Math.random() * 10);
+            System.out.println(array2.array[j]);
+        }
 
     }
 }
