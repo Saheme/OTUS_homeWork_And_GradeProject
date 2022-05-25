562a58955eabd3fdf275628947411d43355dc67f
Saheme <roj_t@mail.ru>
2022-05-25 18:11:44 +0300

    1-й объект сортировка java с выводом

--- src/Lesson11/ArrayTest.java
+++ src/Lesson11/ArrayTest.java
@@ -4,24 +4,23 @@ import java.util.Arrays;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        Numbers array1 = new Numbers(new int[10]);
+        Numbers array1 = new Numbers(new int[20]);
         for (int i = 0; i <array1.array.length; i++){
             array1.array[i] =(int)(Math.random() * 10);
         }
-            System.out.print(Arrays.toString(array1.array));
-
-
+            System.out.print("Изначальный массив" + " " + Arrays.toString(array1.array));
 
         System.out.println();
+
         Arrays.sort(array1.array);
-        System.out.println(Arrays.toString(array1.array));
+        System.out.println("Стандартная сортировка Java" + " " + Arrays.toString(array1.array));
 
         System.out.println();
-        Numbers array2 = new Numbers(new int[10]);
+        Numbers array2 = new Numbers(new int[20]);
         for (int j = 0; j <array2.array.length; j++){
             array2.array[j] =(int)(Math.random() * 10);
-            System.out.print(array2.array[j] + " ");
         }
+            System.out.print("Изначальный массив" + " " + Arrays.toString(array2.array));
 
 
     }
