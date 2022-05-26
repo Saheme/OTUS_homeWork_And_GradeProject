6152fe7bcdc6a3ac9c9c94ae41a0dac0d7572b2b
Saheme <roj_t@mail.ru>
2022-05-26 19:00:40 +0300

    рабочий вариант с 2-я сортировками и плохим счетчиком времени

--- src/Lesson11/ArrayTest.java
+++ src/Lesson11/ArrayTest.java
@@ -1,52 +1,65 @@
 package Lesson11;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        Numbers array1 = new Numbers(new int[10]);
-        for (int i = 0; i < array1.array.length; i++) {
-            array1.array[i] = (int) (Math.random() * 10);
-        }
-        System.out.print("Изначальный массив" + " " + Arrays.toString(array1.array));
+        List<Integer> array1 = new ArrayList<>(10);
+        array1.add(5);
+        array1.add(7);
+        array1.add(0);
+        array1.add(9);
+        array1.add(3);
+        array1.add(7);
+        array1.add(8);
+        array1.add(4);
+        array1.add(2);
+        array1.add(1);
+        System.out.println("Изначальный массив" + " " + array1);
 
-        System.out.println();
         long start = System.nanoTime();
-
-        Arrays.sort(array1.array);
+        Collections.sort(array1);
         long stop = System.nanoTime();
-        System.out.println("Стандартная сортировка Java" + " " + Arrays.toString(array1.array));
+        System.out.println("Стандартная сортировка Java" + " " +  array1);
         System.out.println(stop - start);
 
-        System.out.println();
-        Numbers array2 = new Numbers(new int[10]);
-        for (int i = 0; i < array2.array.length; i++) {
-            array2.array[i] = (int) (Math.random() * 10);
-        }
-        System.out.print("Изначальный массив" + " " + Arrays.toString(array2.array));
-        System.out.println();
+        List<Integer> array2 = new ArrayList<>(10);
+        array2.add(3);
+        array2.add(9);
+        array2.add(4);
+        array2.add(2);
+        array2.add(8);
+        array2.add(5);
+        array2.add(6);
+        array2.add(1);
+        array2.add(7);
+        array2.add(5);
+        System.out.println("Изначальный массив" + " " + array2);
+
         long start2 = System.nanoTime();
         int bufer;
         boolean statusSort = false;
         while (!statusSort) {
             statusSort = true;
-            for (int i = 1; i < array2.array.length; i++) {
-                if (array2.array[i] < array2.array[i - 1]) {
-                    bufer = array2.array[i];
-                    array2.array[i] = array2.array[i - 1];
-                    array2.array[i - 1] = bufer;
+            for (int i = 1; i < array2.size(); i++) {
+                if (array2.get(i) < array2.get(i - 1)) {
+                    bufer = array2.get(i);
+                    array2.set(i, array2.get(i - 1));
+                    array2.set(i - 1, bufer);
                     statusSort = false;
                 }
 
             }
-            System.out.println("Пузырьковая сортировка " + " " + Arrays.toString(array2.array));
+            System.out.println("Пузырьковая сортировка " + " " + array2);
         }
-            long stop2 = System.nanoTime();
-            System.out.println(stop2 - start2);
+        long stop2 = System.nanoTime();
+        System.out.println(stop2 - start2);
+
 
 
     }
 }
-
 
 
